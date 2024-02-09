84272e6a45d1e3c66f82ff7309b8e804f341635a
Nyambi Acho <achonyambi@gmail.com>
2024-02-09 08:38:15 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Tag, Post 
+from blog.models import Tag, Post, Comment
 
 
 
@@ -12,3 +12,4 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
