19f89eee7d792f119405012ec3ef86a5b499a21b
Nyambi Acho <achonyambi@gmail.com>
2024-02-11 22:14:55 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blango.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
