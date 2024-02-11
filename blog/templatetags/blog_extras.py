1df890873c0b28d313fe8cc487b91a8456d16fad
Nyambi Acho <achonyambi@gmail.com>
2024-02-11 17:49:05 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -5,9 +5,42 @@ from django.utils.safestring import mark_safe
 from django.contrib.auth import get_user_model 
 user_model = get_user_model()
 
+from blog.models import Post
+
 register = template.Library()
 
-@register.filter
+
+@register.inclusion_tag('blog/post-list.html')
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[:5]
+  return {"title": "Recent Posts", "posts": posts}
+
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
+
+@register.filter(takes_context=True)
 def author_details(author, current_user=None): 
   if not isinstance(author, user_model): 
     # return empty string as safe default 
@@ -31,3 +64,21 @@ def author_details(author, current_user=None):
     suffix = ""
 
   return mark_safe(f"{prefix}{name}{suffix}")
+
+@register.simple_tag
+def row(extra_classes=""): 
+  return format_html('<div class="row {}"', extra_classes)
+
+@register.simple_tag
+def endrow(): 
+  return format_html("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=""): 
+  return format_html('<div class="col {}"', extra_classes)
+
+@register.simple_tag
+def endcol(): 
+  return format_html("</div>")
+
